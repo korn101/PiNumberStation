9e855a13f7ce26e25ef438443710bb42c042a417
korn101 <jaime2psp@gmail.com>
2014-12-23 23:33:08 +0200

    Commit on Tue Dec 23 23:33:08 SAST 2014 from root

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -3,6 +3,6 @@ git add readme.md
 git add test.sh
 git add PiNS.py
 git add gitUpdate.sh
-git commit -m "Commit at $(date)"
+git commit -m "Commit on $(date) from $(whoami)"
 git push -u origin master
 
