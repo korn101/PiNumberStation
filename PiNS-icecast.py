bf7286bd5f50613740639958437b64f9a8463fb8
Mirko Chialastri <chialastri.mirko@gmail.com>
2019-06-24 23:56:32 +0200

    Multiple icecast radio with custom directory

--- PiNS-icecast.py
+++ PiNS-icecast.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python
-#
+# -*- coding: utf-8 -*-
 # PiNumberStation over icecast 
 #
 # Requires:
+#
 # 	- icecast 		 (https://icecast.org/download | brew install icecast)
 #	- liquidsoap 	 (https://www.liquidsoap.info/doc-dev/install.html)
 #	- liquidsoap-cry (liquidsoup plugin for stream to icecast)
 #
 # Configure:
+#
 # 	- 1. (required) Setup icecast: edit ```/etc/icecast.xml```, ```/usr/local/etc/icecast.xml```
 #	- 2. (required) Run icecast: run ```icecast -c /usr/local/etc/icecast.xml -b```
-#	- 3. (optional) Setup message: edit ```default.ini``` section streaming
+#	- 3. (optional) Setup PiNS steaming: edit ```default.ini``` section streaming
 #   - 4. (required) Setup liquidsoap config: edit ```streaming/config.liq``` with your host, and port (the same of icecast.xml)
 #   - 5. (optional) Setup trasmission: edit ```streaming/stream.liq```
 #
 # Usage:
 #
-# ./PiNS-icecast.py --text "my message"
-# ./PiNS-icecast.py --text "my message" --key "key for encrypt text with Vernam's Cipher"
+# ./PiNS-icecast.py 
+#		--text your_message 
+#		[--key "your_encrypt_key"] 
+#		[-s [default]]
+#
+# Stream normal message:
+# 	./PiNS-icecast.py --text "my message" --key "key for encrypt text with Vernam's Cipher"
+#
+# Stream encrypted message (use --key)
+# 	./PiNS-icecast.py --text "my message" --key "key for encrypt text with Vernam's Cipher"
+#
+# Set trasmission config
+# 	./PiNS-icecast.py --text "foo" --stream daily
+# 	./PiNS-icecast.py --text "foo" --stream night
 #
 import sys
 import subprocess # for calling console
@@ -28,15 +42,15 @@ from random import choice
 from distutils.spawn import find_executable
 
 import argparse
-default_trasmission=sys.path[0] + "/streaming/default.liq"
+default_trasmission=sys.path[0] + "/streaming/default/main.liq"
 
-parser = argparse.ArgumentParser(prog="enc", add_help=True, description='Encrypt text with VernamCipher')
+parser = argparse.ArgumentParser(prog="PiNS-icecast.py", add_help=True, description='Encrypt text with VernamCipher')
 parser.add_argument('--conf', '--config', '-c', metavar='config', default="default.ini", type=str, help='Config file')
 parser.add_argument('--text', '--string', '-t', metavar='text', default="hello", type=str, help='text to crypt')
 parser.add_argument('--key',  '--pass',   '-k', metavar='key', default="", type=str, help='Encrypt text with VernamCipher')
 parser.add_argument('--stream', '--trasmission', '-s', metavar="stream", type=str, default=default_trasmission, help="Your stream name")
 args = parser.parse_args()
-args.stream = args.stream if args.stream.endswith('.liq') else args.stream + ".liq"
+args.stream = args.stream if args.stream.endswith('.liq') else args.stream + "/main.liq"
 
 
 config = ConfigParser.ConfigParser()
@@ -67,7 +81,7 @@ def getVO( character ):
 
 
 def constructWav( strMessage ):
-	print("Synthesizing Message..")
+	print(">> Synthesizing Message..")
 
 	infiles = []
 	strMessageOut = strMessage
@@ -117,18 +131,20 @@ def constructWav( strMessage ):
 		output.writeframes(data[x][1])
 
 	output.close()
-	print("Synthesis Complete..")
+	print("<< Synthesis Complete..")
 
 	return
 
+"""Run LiquidSoap"""
 def stream_message():
 	liquidsoap_bin = config.get('streaming', 'liquidsoap_bin').replace('"', '').strip()
 	liquidsoap_bin = os.path.expanduser(liquidsoap_bin)
 
 	if not find_executable(liquidsoap_bin):
-		raise Exception("%s not exists... edit your .ini (key streaming.liquidsoap_bin)" % liquidsoap_bin)
+		raise Exception("❌	%s not exists... edit your .ini (key streaming.liquidsoap_bin)" % liquidsoap_bin)
 
 
+	stream_cfg = args.stream
 	if not os.path.isabs(args.stream):
 		stream_cfg  = os.path.dirname(__file__) + "/streaming/" + args.stream
 
@@ -136,13 +152,24 @@ def stream_message():
 	stream_file = os.path.basename(stream_cfg)
 	stream_dir  = os.path.dirname(stream_cfg)
 
+
+	if not os.path.exists(stream_dir):
+		print "ℹ️	Copy default config with:"
+		print "		mkdir -p %s" % (stream_dir)
+		raise Exception("❌	Directory %s not exists" % (stream_dir))
+
 	if not os.path.exists(stream_cfg):
-		raise Exception("%s not founded... (cp %s %s)" % ( stream_file, stream_dir + "/default.liq", stream_cfg))
+		print "ℹ️	Copy main config with:"
+		print "		cp %s %s/main.liq" % ( sys.path[0] + "/streaming/default/main.liq", stream_dir)
+		raise Exception("❌	%s not founded..." % (stream_file))
 
 	print("Starting process: %s %s" % (liquidsoap_bin, stream_cfg))
 	subprocess.call([liquidsoap_bin, stream_cfg])
 
 if __name__ == "__main__":
+	if not os.path.exists(args.conf):
+		raise Exception("❌	config %s not exists", args.conf)
+
 	config.read(args.conf)
 
 	if len(args.key) > 0:
@@ -155,7 +182,8 @@ if __name__ == "__main__":
 
 
 	if not message:
-		raise Exception("Empty PiNumberStation message")
+		raise Exception("❌ Empty message (use --text string)")
 
 	constructWav(message)
 	stream_message()
+
