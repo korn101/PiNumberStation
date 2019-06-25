37a59b4d22b2c89b0000eb9bb78854d601cbd93e
Mirko Chialastri <chialastri.mirko@gmail.com>
2019-06-25 04:20:05 +0200

    output and default name

--- streaming/bin/new-stream.sh
+++ streaming/bin/new-stream.sh
@@ -6,11 +6,22 @@ file=$(test -L "$0" && readlink "$0" || echo "$0")
 echo "Name of strem (slug)"
 read -r name
 
-name="test"
-dir_from="$(pwd $0)/default/"
+if [[ $name == "" ]]; then
+	name="new"
+fi
+
+dir_from="$(pwd $0)/default"
 dir_to="${dir_from/default/$name}"
 
 cp -R $dir_from/ $dir_to
+echo "" > $dir_to/liquidsoup.log
+
+echo "Path substition: "
+echo " >> from: $dir_from"
+echo " >> to: $dir_to"
+
 find $dir_to -name '*.liq' -type f -print0  | xargs -0 sed -i._back "s#${dir_from}#${dir_to}#g"
 find $dir_to -name '*.lst' -type f -print0  | xargs -0 sed -i._back "s#${dir_from}#${dir_to}#g"
 find $dir_to -name '*._back' -type f | xargs rm 
+
+echo "Edit ${dir_to}/config.liq"
