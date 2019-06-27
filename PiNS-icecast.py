1c3b4549ade74affe88c04a93effbf4c07ff79b1
Mirko Chialastri <chialastri.mirko@gmail.com>
2019-06-27 22:31:09 +0200

    refactory: PiNumberStation class

--- PiNS-icecast.py
+++ PiNS-icecast.py
@@ -20,7 +20,7 @@
 #
 # ./PiNS-icecast.py 
 #		--text your_message 
-#		[--key "your_encrypt_key"] 
+#		[--key "your_encrypt_key"] [--key-rand]
 #		[-s [default]]
 #
 # Stream normal message:
@@ -41,164 +41,200 @@ import ConfigParser
 from random import choice
 from distutils.spawn import find_executable
 from string import ascii_letters
-
 import argparse
-default_trasmission=sys.path[0] + "/streaming/default/main.liq"
 
-parser = argparse.ArgumentParser(prog="PiNS-icecast.py", add_help=True, description='Encrypt text with VernamCipher')
+directory=os.path.expanduser(sys.path[0])
+default_trasmission=directory + "/streaming/default/main.liq"
+
+parser = argparse.ArgumentParser(prog="PiNS-icecast.py", add_help=True, description="PiNumberStation over icecast")
 parser.add_argument('--conf', '--config', '-c', metavar='config', default="default.ini", type=str, help='Config file')
 parser.add_argument('--text', '--string', '-t', metavar='text', default="hello", type=str, help='text to crypt')
 parser.add_argument('--key',  '--pass',   '-k', metavar='key', default="", type=str, help='Encrypt text with VernamCipher')
 parser.add_argument('--key-rand',  '--pass-rand',  metavar='key_random', const=True, nargs='?', default=False, type=bool, help='Encrypt text with VernamCipher')
 parser.add_argument('--stream', '--trasmission', '-s', metavar="stream", type=str, default=default_trasmission, help="Your stream name")
 
-args = parser.parse_args()
-args.stream = args.stream if args.stream.endswith('.liq') else args.stream + "/main.liq"
-args.key    = "".join([choice(ascii_letters).lower() for char in args.text]) if args.key_rand else args.key
-
-
-config = ConfigParser.ConfigParser()
-# Sounds for digits/numbers.
-sounds = ["zero.wav", "one.wav", "two.wav", "three.wav", "four.wav", "five.wav", "six.wav", "seven.wav", "eight.wav", "nine.wav"]
-
-# Sounds for alphanumeric. Use NATO Phonetic
-alpha = [
-	"alpha", "bravo", "charlie", "delta",  "echo", "foxtrot", "golf",
-	"hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar",
-	"papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor",
-	"whiskey", "x-ray", "yankee", "zulu"
-]
-
-def getVO( character ):
-	if character.isdigit() == True:
-		return sounds[int(character)]
+"""Get abs path"""
+def resolve_path(path):
+	path = os.path.expanduser(path)
+	if os.path.isabs(path):
+		pass
 	else:
-		if character == ',' or character == ' ':
-			return "_comma.wav"
-		if character == '.':
-			return "_period.wav"
-		if character == '\n':
-			return "nova.wav"
+		path = directory +"/"+ path
 
-		if character.isalpha() == True:
-			return "/alpha/" + str(alpha[ord(character) - ord('a')] + ".wav")
+	return path
 
 
-def constructWav( strMessage ):
-	print(">> Synthesizing Message..")
+class PiNumberStationConfig:
 
-	infiles       = []
-	strMessageOut = strMessage
+	config = ConfigParser.ConfigParser()
+	args   = {}
 
-	# determine infiles for message.
+	sound  = ["zero.wav", "one.wav", "two.wav", "three.wav", "four.wav", "five.wav", "six.wav", "seven.wav", "eight.wav", "nine.wav"]
+	alpha  = [
+		"alpha", "bravo", "charlie", "delta",  "echo", "foxtrot", "golf",
+		"hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar",
+		"papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor",
+		"whiskey", "x-ray", "yankee", "zulu"
+	]
 
-	for file in config.get('streaming', 'prepend').split(","):
-		if not file[0] == "/" or not file[0] == ".":
-			file = sys.path[0] + "/" + file
+	def __init__(self, *args):
+		self.args = args
 
-		if os.path.exists(file):
+	def loadConfig(self, configFile):
+		if not os.path.exists(configFile):
+			raise Exception("❌	config %s not exists", configFile)
+
+		self.config.read(configFile)
+
+	"""Encrypt text"""
+	def encrypt(self, text, key=None):
+		response = {"text": text, "key": key, "encrypt": None}
+
+		if key:
+			process = subprocess.Popen([sys.path[0] + "/encoder.py", "--enc-text", args.text, "--enc-key", args.key], stdout=subprocess.PIPE)
+			result = process.stdout.readlines()[-1]
+			response["encrypt"] = result.rstrip()
+
+		return response
+
+	"""Vocal message from text"""
+	def constructWav(self, message, pwd=None):
+		message    = self.encrypt(message, pwd)
+		strMessage = message["encrypt"]
+
+		if not strMessage:
+			raise Exception("❌ Empty message (use --text string)")
+
+		print(">> Synthesizing Message.. [key=%s]" % (args.key))
+
+		infiles       = []
+		strMessageOut = strMessage
+
+		# determine infiles for message.
+
+		for file in self.config.get('streaming', 'prepend').split(","):
+			if not file[0] == "/" or not file[0] == ".":
+				file = sys.path[0] + "/" + file
+
+			if os.path.exists(file):
+				infiles.append(file)
+			else:
+				print  "File %s not exists ... skipped!" % file
+
+		for character in strMessageOut:
+			charSound  = self.getVO(character)
+			char_sound = directory + "/vo/" + charSound
+			infiles.append(char_sound)
+
+			print "\t %s => %s" % (character, charSound)
+
+		for file in self.config.get('streaming', 'append').split(","):
+			if not file[0] == "/" or not file[0] == ".":
+				file = sys.path[0] + "/" + file
+
+			if os.path.exists(file):
+				infiles.append(file)
+			else:
+				print  "File %s not exists ... skipped!" % file
+
 			infiles.append(file)
+
+		infiles.append(sys.path[0] + "/vo/off3.wav")
+
+		data = []
+		for infile in infiles:
+		    w = wave.open(infile, 'rb')
+		    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+		    w.close()
+
+
+		if os.path.isabs(args.stream):
+			outprefix = os.path.dirname(args.stream)
 		else:
-			print  "File %s not exists ... skipped!" % file
+			outprefix = directory + "/streaming/" + os.path.dirname(args.stream)
 
-	for character in strMessageOut:
-		char_sound = sys.path[0] + "/vo/" + getVO(character)
-		infiles.append(char_sound)
-		print "\t %s => %s" % (character, getVO(character))
+		outfile = outprefix + "/message.wav"
+		if not os.path.exists(outfile):
+			f=open(outfile, 'w+')
+			f.write("")
+			f.close()
+			
+		output = wave.open(outfile, 'wb')
+		output.setparams(data[0][0])
 
-	for file in config.get('streaming', 'append').split(","):
-		if not file[0] == "/" or not file[0] == ".":
-			file = sys.path[0] + "/" + file
+		for x in range(0, len(infiles)):
+			output.writeframes(data[x][1])
 
-		if os.path.exists(file):
-			infiles.append(file)
+		output.close()
+		print("<< Synthesis Complete..")
+
+		return
+
+	"""Sound from text"""
+	def getVO(self, character):
+		if character.isdigit() == True:
+			return self.sound[int(character)]
 		else:
-			print  "File %s not exists ... skipped!" % file
+			if character == ',' or character == ' ':
+				return "_comma.wav"
+			if character == '.':
+				return "_period.wav"
+			if character == '\n':
+				return "nova.wav"
 
-		infiles.append(file)
+			if character.isalpha() == True:
+				return "/alpha/" + str(self.alpha[ord(character) - ord('a')] + ".wav")
 
-	infiles.append(sys.path[0] + "/vo/off3.wav")
+	"""Run LiquidSoap"""
+	def stream_message(self):
+		liquidsoap_bin = self.config.get('streaming', 'liquidsoap_bin').replace('"', '').strip()
+		liquidsoap_bin = os.path.expanduser(liquidsoap_bin)
 
-	if not os.path.isabs(args.stream):
-		stream_cfg  = os.path.dirname(__file__) + "/streaming/" + args.stream
-
-	data = []
-	for infile in infiles:
-	    w = wave.open(infile, 'rb')
-	    data.append( [w.getparams(), w.readframes(w.getnframes())] )
-	    w.close()
-
-
-	outfile = os.path.dirname(args.stream) + "/message.wav"
-	if not os.path.isabs(outfile):
-		outfile = os.path.dirname(__file__) + "/streaming/" + outfile
-
-	outfile = os.path.abspath(os.path.expanduser(outfile))
-	
-	if not os.path.exists(outfile):
-		f=open(outfile, 'w+')
-		f.close()
-		
-	output = wave.open(outfile, 'wb')
-	output.setparams(data[0][0])
-
-	for x in range(0, len(infiles)):
-		output.writeframes(data[x][1])
-
-	output.close()
-	print("<< Synthesis Complete..")
-
-	return
-
-"""Run LiquidSoap"""
-def stream_message():
-	liquidsoap_bin = config.get('streaming', 'liquidsoap_bin').replace('"', '').strip()
-	liquidsoap_bin = os.path.expanduser(liquidsoap_bin)
-
-	if not find_executable(liquidsoap_bin):
-		raise Exception("❌	%s not exists... edit your .ini (key streaming.liquidsoap_bin)" % liquidsoap_bin)
+		if not find_executable(liquidsoap_bin):
+			raise Exception("❌	%s not exists... edit your .ini (key streaming.liquidsoap_bin)" % liquidsoap_bin)
 
 
-	stream_cfg = args.stream
-	if not os.path.isabs(args.stream):
-		stream_cfg  = os.path.dirname(__file__) + "/streaming/" + args.stream
-
-	stream_cfg  = os.path.abspath(os.path.expanduser(stream_cfg))
-	stream_file = os.path.basename(stream_cfg)
-	stream_dir  = os.path.dirname(stream_cfg)
+		stream_cfg  = os.path.abspath(os.path.expanduser(args.stream))
+		stream_file = os.path.basename(stream_cfg)
+		stream_dir  = os.path.dirname(stream_cfg)
 
 
-	if not os.path.exists(stream_dir):
-		print "ℹ️	Copy default config with:"
-		print "		mkdir -p %s" % (stream_dir)
-		raise Exception("❌	Directory %s not exists" % (stream_dir))
+		if not os.path.exists(stream_dir):
+			print "ℹ️	Copy default config with:"
+			print "		mkdir -p %s" % (stream_dir)
+			raise Exception("❌	Directory %s not exists" % (stream_dir))
 
-	if not os.path.exists(stream_cfg):
-		print "ℹ️	Copy main config with:"
-		print "		cp %s %s/main.liq" % ( sys.path[0] + "/streaming/default/main.liq", stream_dir)
-		raise Exception("❌	%s not founded..." % (stream_file))
+		if not os.path.exists(stream_cfg):
+			print "ℹ️	Copy main config with:"
+			print "		cp %s %s/main.liq" % ( sys.path[0] + "/streaming/default/main.liq", stream_dir)
+			raise Exception("❌	%s not founded..." % (stream_file))
 
-	print("Starting process: %s %s" % (liquidsoap_bin, stream_cfg))
-	subprocess.call([liquidsoap_bin, stream_cfg])
+		print("Starting process: %s %s" % (liquidsoap_bin, stream_cfg))
+		subprocess.call([liquidsoap_bin, stream_cfg])
+
+	def get_stream(self):
+		return os.path.dirname(args.stream).split("/")[-1]
 
 if __name__ == "__main__":
-	if not os.path.exists(args.conf):
-		raise Exception("❌	config %s not exists", args.conf)
+	args = parser.parse_args()
 
-	config.read(args.conf)
+	args.stream = os.path.expanduser(args.stream if args.stream.endswith('.liq') else args.stream + "/main.liq")
+	if not os.path.isabs(args.stream):
+		args.stream = "streaming/%s" % args.stream
 
-	if len(args.key) > 0:
-		process=subprocess.Popen([sys.path[0] + "/encoder.py", "--enc-text", args.text, "--enc-key", args.key], stdout=subprocess.PIPE)
-		result=process.stdout.readlines()[-1]
-		print(result)
-		message=result.rstrip()
-	else:
-		message=args.text
+	# random key from text len
+	args.key    = "".join([choice(ascii_letters).lower() for char in args.text]) if args.key_rand else args.key
 
+	# expand path's
+	for key in ['conf', 'stream']:
+		try:
+			value = getattr(args, key)
+			setattr(args, key, resolve_path(value))
+		except e:
+			pass
 
-	if not message:
-		raise Exception("❌ Empty message (use --text string)")
-
-	constructWav(message)
-	stream_message()
-
+	# run PiNS
+	NStation = PiNumberStationConfig(vars(args))
+	NStation.loadConfig(args.conf)
+	NStation.constructWav(args.text, args.key)
+	NStation.stream_message()
